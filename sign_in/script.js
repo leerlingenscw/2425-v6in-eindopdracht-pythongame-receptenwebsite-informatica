2b03347a5867641022ac10b383b7945d9da0f6dc
irenepool <133474@stanislascollege.net>
2025-02-08 11:55:47 +0000

    js

--- sign_in/script.js
+++ sign_in/script.js
@@ -1,28 +1,25 @@
-var loginForm = document.getElementById('loginForm');
-if (loginForm) {
-    loginForm.addEventListener('submit', function (event) {
-        var username = document.getElementById('username').value;
-        var email = document.getElementById('email').value;
-        var password = document.getElementById('password').value;
-        
-        if (username === '' || email === '' || password === '') {
-            alert('Alle velden zijn verplicht!');
-            event.preventDefault(); 
-        }
-    });
-}
+
+document.getElementById('loginForm').addEventListener('submit', function (event) {
+    var username = document.getElementById('username').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    
+    if (username === '' || email === '' || password === '') {
+        alert('Alle velden zijn verplicht');
+        event.preventDefault();
+    }
+    
+});
 
 
-var registerForm = document.getElementById('registerForm');
-if (registerForm) {
-    registerForm.addEventListener('submit', function (event) {
-        var username = document.getElementById('username').value;
-        var email = document.getElementById('email').value;
-        var password = document.getElementById('password').value;
-        
-        if (username === '' || email === '' || password === '') {
-            alert('Alle velden zijn verplicht!');
-            event.preventDefault(); 
-        }
-    });
-}
+document.getElementById('registerForm').addEventListener('submit', function (event) {
+    var username = document.getElementById('username').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    
+    if (username === '' || email === '' || password === '') {
+        alert('Alle velden zijn verplicht');
+        event.preventDefault();
+    }
+    
+});
