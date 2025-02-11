c8ee2ab0ae36d6b5871af1e91ea5d982e125fb76
irenepool <133474@stanislascollege.net>
2025-02-11 14:56:35 +0000

    js

---
+++ sign_in/register.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+  
+    document.getElementById('registerForm').addEventListener('submit', function (event) {
+        const username = document.getElementById('username').value;
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+  
+        if (username === '' || email === '' || password === '') {
+            alert('Alle velden zijn verplicht');
+            event.preventDefault(); 
+        }
+    });
+});
