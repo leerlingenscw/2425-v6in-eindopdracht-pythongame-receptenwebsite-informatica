b007d834bc65200545c77b9e86137d95c3b914d5
sannekneppers <133388@stanislascollege.net>
2025-03-03 15:52:12 +0000

    s

--- website/wedstrijd.php
+++ website/wedstrijd.php
@@ -1,16 +1,25 @@
 <?php
-session_start();
-
 $db_path = '/workspaces/2425-v6in-eindopdracht-pythongame-receptenwebsite-informatica/wedstrijd.db';
 $db = new SQLite3($db_path);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $gerecht = trim($_POST['gerecht'] ?? '');
 
-
-    $stmt = $db->prepare("SELECT wedstrijd_id, gerecht);
+    $stmt = $db->prepare("SELECT COUNT(*) as count FROM wedstrijd WHERE gerecht = :gerecht");
     $stmt->bindValue(':gerecht', $gerecht, SQLITE3_TEXT);
     $result = $stmt->execute();
-    $user = $result->fetchArray(SQLITE3_ASSOC);
+    $row = $result->fetchArray(SQLITE3_ASSOC);
+
+    if ($row['count'] > 0) {
+        header("Location: wedstrijd.html?error=gerecht");
+        exit();
+    }
+
+    $stmt = $db->prepare("INSERT INTO wedstrijd (gerecht) VALUES (:gerecht)");
+    $stmt->bindValue(':gerecht', $gerecht, SQLITE3_TEXT);
+
+    if ($stmt->execute()) {
+        header("Location: /website/wedstrijduploaden.html");
+    }
 }
 ?>
